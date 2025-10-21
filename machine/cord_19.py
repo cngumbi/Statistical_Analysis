39c21837bf1027de50ffd300eadf9df7c69075f4
cngumbi <cngumbi35@gmail.com>
2025-10-21 19:03:39 +0300

    restructuring the project

--- machine/cord_19.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as ps
-import numpy as ny 
-
-#read the csv file 
-data  = ps.read_csv('Mikoko1.csv')
-
-print(data)
--- machine/mat.py
+++
@@ -1,4 +0,0 @@
-#funtions to compare two things
-#this module will comtain
-def cmp(a,b):
-    return (a > b) - (a < b)
